b98513adbdcec5405f737f3a41e6b2a45fa588fd
shadowedge <gulixing@msn.com>
2013-07-14 16:50:06 +0800

    Signed-off-by: shadowedge <gulixing@msn.com>

---
+++ dao/src/test/java/haha/dao/UserServiceTest.java
@@ -0,0 +1,50 @@
+package haha.dao;
+/**                                
+ * Copyright ® 2013 东软集团股份有限公司
+ * 版权所有。     
+ */
+
+import haha.base.AbstractTest;
+import junit.framework.Assert;
+import keter.KeterAbstractServiceTest;
+import keter.domain.User;
+import keter.service.UserService;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * <p>
+ * Class : test.persistence.HahaDaoTest
+ * <p>
+ * Descdription: 类功能描述
+ * 
+ * @author 顾力行-gulixing@msn.com
+ * @version 1.0.0
+ */
+public class UserServiceTest extends AbstractTest {
+
+	@Autowired
+	private UserService service;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void test(){
+		User user = new User();
+		user.setAccount("a");
+		user.setUsername("我");
+		user.setPassword("1");
+		service.add(user);
+		Assert.assertEquals(1,service.all().size());
+	}
+	
+}
