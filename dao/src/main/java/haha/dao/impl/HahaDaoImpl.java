c83eb66dbb325e466d83ff39a6fdb31aba35618d
shadowedge <gulixing@msn.com>
2013-07-15 21:18:27 +0800

    Signed-off-by: shadowedge <gulixing@msn.com>

--- dao/src/main/java/haha/dao/impl/HahaDaoImpl.java
+++ dao/src/main/java/haha/dao/impl/HahaDaoImpl.java
@@ -1,23 +1,20 @@
-
 package haha.dao.impl;
 
 import haha.dao.HahaDao;
 import haha.domain.Haha;
 import keter.dao.base.KeterAbstractDao;
-import keter.domain.User;
 
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public class HahaDaoImpl extends KeterAbstractDao<Haha>  implements HahaDao {
-	
-	public void ex(){
+public class HahaDaoImpl extends KeterAbstractDao<Haha> implements HahaDao {
+
+	public void ex() {
 		System.out.println("我要异常了：");
-	    int a[] = new int[1];
-	    System.out.println(a[1]);//制造异常
+		int a[] = new int[1];
+		System.out.println(a[1]);// 制造异常
 	}
-	
+
 	public void exceptionMock() {
 		throw new RuntimeException("I'm Exception!");
 	}
