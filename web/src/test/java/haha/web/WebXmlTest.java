4ecee4851a5fcbeb6f1c9ff1135fdeb60c633fc5
shadowedge <gulixing@msn.com>
2013-07-30 20:55:10 +0800

    Signed-off-by: shadowedge <gulixing@msn.com>

--- web/src/test/java/haha/web/WebXmlTest.java
+++ web/src/test/java/haha/web/WebXmlTest.java
@@ -6,6 +6,13 @@ import keter.web.util.XMLValidator;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+/**
+ * <p>Class       : haha.web.WebXmlTest
+ * <p>Descdription: 测试工程的web.xml文件是否符合JavaEE规范
+ *				          只有符合规范才有可能顺利在Weblogic下发布
+ * @author  gulixing@msn.com
+ * @version 1.0.0
+ */
 public class WebXmlTest {
 	/**
 	 * Logger for this class
@@ -16,8 +23,9 @@ public class WebXmlTest {
 	public void test() {
 		logger.info(PathUtil.getProjectRoot()); //file:/E:/git/keter/keter-project/web
 		String root = PathUtil.getProjectRoot();
-		String xmlFileName = root+"/src/main/webapp/WEB-INF/web.xml";
-	    String xsdFileName = root+"/src/main/webapp/WEB-INF/xsd/web-app_2_5.xsd";
+		String webinf = root+"/src/main/webapp/WEB-INF/";
+		String xmlFileName = webinf+"web.xml";
+	    String xsdFileName = webinf+"/xsd/web-app_2_5.xsd";
 	    XMLValidator.validateXMLByXSD(xmlFileName,xsdFileName);
 	}
 }
