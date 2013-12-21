2ea83e0ef04153a7f48208d86f50eecf885d89ab
gu <gu@Shadowedge-Home>
2013-12-21 19:37:43 +0800

    Signed-off-by: gu <gu@Shadowedge-Home>

--- web/src/test/java/haha/web/JndiDataSourceTest.java
+++ web/src/test/java/haha/web/JndiDataSourceTest.java
@@ -6,6 +6,13 @@ import keter.web.util.JndiDataSourceValidator;
 
 import org.junit.Test;
 
+/**
+ * <p>Class       : haha.web.JndiDataSourceTest
+ * <p>Descdription: 测试Weblogic数据源可否连接成功
+ *
+ * @author  gulixing@msn.com
+ * @version 1.0.0
+ */
 public class JndiDataSourceTest {
 
 	@Test
